80d9b08fc866ff5fd3b5a85de13a8d7ed31a330a
israbq <66181763+israbq@users.noreply.github.com>
2025-12-02 23:20:44 +0000

    Prueba 2 - Quitando ADMIN

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ BOT_TOKEN = os.environ["BOT_TOKEN"]  # asegúrate de crearla en Secrets
 
 WARNINGS_FILE = "warnings.json"
 MAX_WARNINGS = 3
-DELETE_AFTER_SECONDS = 10  # 5 minutos
+DELETE_AFTER_SECONDS = 10  # 2 minutos
 # -------------------------------------------
 
 # Inicializar bot
@@ -171,13 +171,13 @@ async def check_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
     key = f"{chat_id}:{user_id}"
 
     # Ignorar admins / creador
-    try:
-        member = await context.bot.get_chat_member(chat_id, user_id)
-        if member.status in ["administrator", "creator"]:
-            return
-    except Exception as e:
-        print(f"Error en get_chat_member: {e}")
-        return
+    #try:
+     #   member = await context.bot.get_chat_member(chat_id, user_id)
+      #  if member.status in ["administrator", "creator"]:
+       #     return
+    #except Exception as e:
+     #   print(f"Error en get_chat_member: {e}")
+      #  return
 
     if contains_link(message):
         # 1) Intentar borrar el mensaje del usuario
